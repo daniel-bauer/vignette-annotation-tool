c738fc34fb1425462cbbd3ae242755fc7e16e0ca
Daniel Bauer <bauer@cs.columbia.edu>
2013-02-25 15:13:16 -0500

    Moved old code by Daniel to a legacy directory.

---
+++ old/vignet/bow.lisp
@@ -0,0 +1,209 @@
+
+((23 (:VERB "bow") (:DIRECT-OBJECT "one's head")) :TIME 3550849495
+ :VIGNETTE-ARGS ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 "NOD-HEAD" :GESTURE))
+ :SEMIOTIC-STATUS "literal" :INPUT-ARGS
+ ((:INPUT-0 :SUBJECT "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :DIRECT-OBJECT "one's_head"))
+ :SUB-RELATIONS
+ ((:WE.GESTURE-AT-TARGET.R (:AGENT :ARG-0) (:TARGET NIL) (:TARGET-PART NIL)
+   (:GESTURE :ARG-1) (:DISTANCE NIL)))
+ :USER 1) 
+((18 (:VERB "bow") (:AUX "to do something"))) 
+((6 (:VERB "bow") (:PP "to pressure")) :VALIDITY :VALID :USER 225 :TIME
+ 3556992390 :SUB-RELATIONS
+ ((:WE.AIM-INSTRUMENT-AT-TARGET.R (:AGENT) (:INSTRUMENT) (:TARGET)
+   (:TARGET-PART) (:TARGET-DISTANCE) (:PROJECTILE) (:PROJECTILE-LOCATION)
+   (:PROJECTILE-PATH)))
+ :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :PP-TO "pressure"))) 
+((4 (:VERB "bow") (:PP "over one's hand")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((3 (:VERB "bow") (:DIRECT-OBJECT "heads")) :USER 225 :TIME 3556992390
+ :VALIDITY :VALID) 
+((2 (:VERB "bow") (:DIRECT-OBJECT "a knee")) :VALIDITY :INVALID :USER 1
+ :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :DIRECT-OBJECT "a_knee"))) 
+((2 (:VERB "bow") (:PP "at a name")) :VALIDITY :INVALID :USER 1 :TIME
+ 3551790383) 
+((2 (:VERB "bow") (:PP "in turn"))) 
+((2 (:VERB "bow") (:PP "to wishes")) :TIME NIL :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow") (:INPUT-2 :PP-TO "wishes"))
+ :VIGNETTE-ARGS NIL :PARENT NIL :USER NIL) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "a neck")) :VALIDITY :VALID :VIGNETTE-ARGS
+ NIL :PARENT NIL :TIME 3556992390 :USER 225) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "caps") (:PP "below stairs")) :VALIDITY
+ :INVALID :USER 1) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "low")) :TIME 3556992390 :VALIDITY :VALID
+ :VIGNETTE-ARGS ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 "BOW" :GESTURE))
+ :SEMIOTIC-STATUS "literal" :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :DIRECT-OBJECT "low"))
+ :SUB-RELATIONS
+ ((:WE.GESTURE-AT-TARGET.R (:AGENT :ARG-0) (:TARGET NIL) (:TARGET-PART NIL)
+   (:GESTURE :ARG-1) (:DISTANCE NIL)))
+ :USER 225) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "more"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's head and a shame")) :INPUT-ARGS
+ ((:INPUT-0 :SUBJECT "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :DIRECT-OBJECT "one's_head_and_a_shame"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's head and drop") (:PP "in adoration"))
+ :USER 1 :TIME 3556131122 :VALIDITY :NONE) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's head") (:PP "before eyes")) :USER 1
+ :TIME 3556637768 :VALIDITY :NONE) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's head") (:PP "in acknowledgement"))
+ :USER 1 :TIME 3556637768 :VALIDITY :NONE :INPUT-ARGS
+ ((:INPUT-0 :SUBJECT "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :DIRECT-OBJECT "one's_head") (:INPUT-3 :PP-IN "acknowledgement"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's head") (:PP "in a direction"))
+ :VIGNETTE-ARGS NIL :USER 1 :TIME 3556664717 :SUB-RELATIONS NIL :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :DIRECT-OBJECT "one's_head") (:INPUT-3 :PP-IN "a_direction"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's head") (:PP "in front"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's head") (:PP "in hands"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's head") (:PP "under a bier"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's leave"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's product")) :VALIDITY :INVALID :USER 1) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "one's way") (:PP "out an inn"))
+ :VIGNETTE-ARGS NIL :SUB-RELATIONS NIL :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :DIRECT-OBJECT "one's_way") (:INPUT-3 :PP-OUT "an_inn"))
+ :USER 1) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "scrape")) :VALIDITY :INVALID :USER 1) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "shoulders")) :USER 225 :TIME 3556992390
+ :VALIDITY :VALID) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "someone") (:PP "in a two")) :USER 225 :TIME
+ 3556992390 :VALIDITY :VALID) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "stiffly") (:PP "to an other"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "thanks"))) 
+((1 (:VERB "bow") (:DIRECT-OBJECT "trees")) :USER 225 :TIME 3556992390
+ :VALIDITY :VALID) 
+((1 (:VERB "bow") (:PP "at a knight") (:PP "beside Corbett")) :TIME 3556992390
+ :VALIDITY :VALID :VIGNETTE-ARGS
+ ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 :INPUT-2 :TARGET) (:ARG-2 "BOW" :GESTURE))
+ :SEMIOTIC-STATUS "literal" :USER 225 :SUB-RELATIONS
+ ((:WE.GESTURE-AT-TARGET.R (:AGENT :ARG-0) (:TARGET :ARG-1) (:TARGET-PART NIL)
+   (:GESTURE :ARG-2) (:DISTANCE NIL)))
+ :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow") (:INPUT-2 :PP-AT "a_knight")
+  (:INPUT-3 :PP-BESIDE "Corbett"))) 
+((1 (:VERB "bow") (:PP "before an onslaught")) :USER 225 :TIME 3556992390
+ :VALIDITY :VALID) 
+((1 (:VERB "bow") (:PP "beneath a weight")) :USER 225 :TIME 3556992390
+ :VALIDITY :VALID) 
+((1 (:VERB "bow") (:PP "from a waist")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "in a curve")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "in a mixture")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "in exhaustion and despair"))) 
+((1 (:VERB "bow") (:PP "in no.")) :VALIDITY :INVALID :USER 1) 
+((1 (:VERB "bow") (:PP "in October"))) 
+((1 (:VERB "bow") (:PP "in one's direction")) :USER 225 :TIME 3556992390
+ :VALIDITY :VALID) 
+((1 (:VERB "bow") (:PP "in one's turn")) :USER 1) 
+((1 (:VERB "bow") (:PP "in prayer")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "in respect")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "in shame and despondency"))) 
+((1 (:VERB "bow") (:PP "into a curve") (:PP "by two")) :USER 225 :TIME
+ 3556992390 :VALIDITY :VALID) 
+((1 (:VERB "bow") (:PP "into a hallway")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "into a water"))) 
+((1 (:VERB "bow") (:PP "in worship")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "like one's longbow"))) 
+((1 (:VERB "bow") (:PP "on something"))) 
+((1 (:VERB "bow") (:PP "out 34th") (:PP "on a green")) :VALIDITY :INVALID :USER
+ 1) 
+((1 (:VERB "bow") (:PP "out a ring"))) 
+((1 (:VERB "bow") (:PP "out a spotlight"))) 
+((1 (:VERB "bow") (:PP "out competition"))) 
+((1 (:VERB "bow") (:PP "out cricket") (:PP "at an end"))) 
+((1 (:VERB "bow") (:PP "out elections") (:PP "after second"))) 
+((1 (:VERB "bow") (:PP "out F1") (:PP "on a note"))) 
+((1 (:VERB "bow") (:PP "out Lotus") (:PP "under one's belt"))) 
+((1 (:VERB "bow") (:PP "out shape"))) 
+((1 (:VERB "bow") (:PP "out training") (:PP "of training"))) 
+((1 (:VERB "bow") (:PP "to age and experience"))) 
+((1 (:VERB "bow") (:PP "to a march")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "to an Emperor")) :TIME 3556992390 :VALIDITY :VALID
+ :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :PP-TO "an_Emperor"))
+ :VIGNETTE-ARGS
+ ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 :INPUT-2 :TARGET) (:ARG-2 "BOW" :GESTURE))
+ :PARENT 74 :USER 225) 
+((1 (:VERB "bow") (:PP "to a night")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "to a rule") (:PP "in one's style"))) 
+((1 (:VERB "bow") (:PP "to a style")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "to a weight"))) 
+((1 (:VERB "bow") (:PP "to a will"))) 
+((1 (:VERB "bow") (:PP "to a woman") (:PP "with a past")) :TIME 3556992390
+ :VALIDITY :VALID :VIGNETTE-ARGS
+ ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 :INPUT-2 :TARGET) (:ARG-2 "BOW" :GESTURE))
+ :PARENT 74 :USER 225) 
+((1 (:VERB "bow") (:PP "to capitalists")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "to conventions")) :USER 225 :TIME 3556992390 :VALIDITY
+ :VALID) 
+((1 (:VERB "bow") (:PP "to cuts"))) 
+((1 (:VERB "bow") (:PP "to EC and pressure"))) 
+((1 (:VERB "bow") (:PP "to few"))) 
+((1 (:VERB "bow") (:PP "to guests")) :TIME 3556992390 :VALIDITY :VALID
+ :CHILDREN (90 84 83 81 68 62) :VIGNETTE-ARGS
+ ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 :INPUT-2 :TARGET) (:ARG-2 "BOW" :GESTURE))
+ :SEMIOTIC-STATUS "literal" :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow") (:INPUT-2 :PP-TO "guests"))
+ :SUB-RELATIONS
+ ((:WE.GESTURE-AT-TARGET.R (:AGENT :ARG-0) (:TARGET :ARG-1) (:TARGET-PART NIL)
+   (:GESTURE :ARG-2) (:DISTANCE NIL)))
+ :USER 225) 
+((1 (:VERB "bow") (:PP "to knees"))) 
+((1 (:VERB "bow") (:PP "to militarists and opposition"))) 
+((1 (:VERB "bow") (:PP "to Moscow"))) 
+((1 (:VERB "bow") (:PP "to needs") (:AUX "to do something"))) 
+((1 (:VERB "bow") (:PP "to none"))) 
+((1 (:VERB "bow") (:PP "to one's decision"))) 
+((1 (:VERB "bow") (:PP "to one's partner")) :TIME 3556992390 :VALIDITY :VALID
+ :VIGNETTE-ARGS
+ ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 :INPUT-2 :TARGET) (:ARG-2 "BOW" :GESTURE))
+ :PARENT 74 :USER 225) 
+((1 (:VERB "bow") (:PP "to one's request"))) 
+((1 (:VERB "bow") (:PP "to one's uncle")) :TIME 3556992390 :VALIDITY :VALID
+ :VIGNETTE-ARGS
+ ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 :INPUT-2 :TARGET) (:ARG-2 "BOW" :GESTURE))
+ :PARENT 74 :USER 225) 
+((1 (:VERB "bow") (:PP "to passengers")) :TIME 3556992390 :VALIDITY :VALID
+ :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :PP-TO "passengers"))
+ :VIGNETTE-ARGS
+ ((:ARG-0 :INPUT-0 :AGENT) (:ARG-1 :INPUT-2 :TARGET) (:ARG-2 "BOW" :GESTURE))
+ :PARENT 74 :USER 225) 
+((1 (:VERB "bow") (:PP "to prejudices"))) 
+((1 (:VERB "bow") (:PP "to pressure") (:PP "to democracy"))) 
+((1 (:VERB "bow") (:PP "to pressures"))) 
+((1 (:VERB "bow") (:PP "to pressures") (:AUX "to do something"))) 
+((1 (:VERB "bow") (:PP "to Primaflora") (:PP "at one's table"))) 
+((1 (:VERB "bow") (:PP "to surgeons")) :VALIDITY :VALID :INPUT-ARGS
+ ((:INPUT-0 :SUBJ "person") (:INPUT-1 :VERB "bow")
+  (:INPUT-2 :PP-TO "surgeons"))
+ :SUB-RELATIONS NIL :VIGNETTE-ARGS NIL :PARENT 74 :TIME 3556992390 :USER 225) 
+((1 (:VERB "bow") (:PP "to technology") (:AUX "to do something"))) 
+((1 (:VERB "bow") (:PP "to Yakovlev") (:PP "along a road")) :VIGNETTE-ARGS NIL
+ :PARENT NIL :TIME 3551790352 :USER 1) 
+((1 (:VERB "bow") (:PP "under one's transgression"))) 
+((1 (:VERB "bow") (:PP "whether political") (:AUX "to do something")) :VALIDITY
+ :INVALID :USER 1) 
+((1 (:VERB "bow") (:PP "with a burden"))) 
+((1 (:VERB "bow") (:PP "with a smile"))) 
+((1 (:VERB "bow") (:PP "with concern") (:PP "in a direction"))) 
+((1 (:VERB "bow") (:PP "with one's cup"))) 
