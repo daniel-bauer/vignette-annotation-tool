790efc780778948771bb49171c673cb9d30fa92d
jettca <jettca1@gmail.com>
2013-03-20 12:58:02 -0400

    Redesigned frame select menu in anticipation of new features

--- annotation_tool/media/newInstance.js
+++ annotation_tool/media/newInstance.js
@@ -1,80 +1,53 @@
-$(document).ready(function(){
-   $('.word-text-box').val(word)
-   getFrames();
-    populateInstances(sceneId,corpusId);
+$(document).ready(function() {
+    $('h1').html('Select frame evoked by "' + word + '"');
+    getFrames();
+
+    // Highlight selected frame, unhighlight previously selected frame
+    var selectedFrameID = '';
+    $('#frame-list').on('click', '.frame', function() {
+        if(selectedFrameID != '') {
+            $('#frame-list li#' + selectedFrameID).removeClass('selected');
+        }
+        $(this).addClass('selected');
+        selectedFrameID = $(this).attr('id');
+    });
 });
 
-function populateInstances(sceneId,corpusId){
-    var dropDownBox = $('#instances-name');
+function getFrames() {
     jQuery.ajax({
-       url: 'http://127.0.0.1:8000/instances/?scene_id=' + sceneId +'&corpus_id=' + corpusId,
-       type: 'get',
-       dataType: 'text',
-       success:function(data)
+        url: 'http://127.0.0.1:8000/frames',
+        type: 'get',
+        dataType: 'text',
+        success:function(data)
         {
-	    instances = data.replace(/&quot;/ig,'"');       
-	    instances = jQuery.parseJSON(instances);
-	    addInstances(instances);
-	}
+            frames = data.replace(/&quot;/ig,'"');       
+            frames = jQuery.parseJSON(frames);
+            populateFrameList(frames);
+        }
     });
 }
 
-function addInstances(instances){
-    var instancesNames = document.getElementById('instances-name');
-    $('#instances-name').html(' ');
-     $.each(instances.instances, function(index, value){
-        instancesNames.add(new Option(value.name,value.name));
-	instancesNames.size = index +1;
-     });
-}
-
-
-function getFrames(){
-    jQuery.ajax({
-       url: 'http://127.0.0.1:8000/frames',
-       type: 'get',
-       dataType: 'text',
-       success:function(data)
-        {
-	   frames = data.replace(/&quot;/ig,'"');       
-	   frames = jQuery.parseJSON(frames);
-	    populateFrameNamesDropDown(frames);
-	}
-    });
-}
-
-function populateFrameNamesDropDown(frames){
-    var dropDownBox = $('#frame-names');
+function populateFrameList(frames) {
+    var frameList = $('#frame-list');
     $.each(frames, function(index,value){
-	var newOption = '<option val='+value.name +'>'+value.name+'</option>';
-	dropDownBox.append(newOption);
+        var newItem = '<li class = "frame" id=' + value.name + '>' + value.name + '</li>';
+        frameList.append(newItem);
     });
 }
 
-function createInstance(){
-    var frameName = $('#frame-names').val();
+function createInstance() {
+    var frameName = $('.selected').attr('id');
     var request_parameters = '?scene_id=' + sceneId +'&corpus_id=' + corpusId + '&word=' + word + '&word_position=' + wordPosition +  '&name=' + frameName + '&sentence_id=' + sentenceId;
     jQuery.ajax({
-	url: 'http://127.0.0.1:8000/create_instances/'+encodeURI(request_parameters),
-       type: 'get',
-       dataType: 'text',
-	success:function(data)
-       {	
-	   alert(data);
-       },
-	error:function(jqXHR)
-	{
-	    alert(jqXHR.status);
-	    alert(jqXHR.responseText);
-	}
-   })
-}
-
-
-function relateInstances(){
-    var selectedInstances = $("#instances-name").val() || [];
-    alert(selectedInstances.length);
-    $.each(selectedInstances, function(index,value){
-	alert(value);
+        url: 'http://127.0.0.1:8000/create_instances/' + encodeURI(request_parameters),
+        type: 'get',
+        dataType: 'text',
+        success:function(data) {	
+            alert(data);
+        },
+        error:function(jqXHR) {
+            alert(jqXHR.status);
+            alert(jqXHR.responseText);
+        }
     });
 }
